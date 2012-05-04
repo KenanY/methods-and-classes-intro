55c8f690f5a043083351acd5c01074eec3986c1a
Kenan Yildirim <kenanyildirim@lavabit.com>
2012-05-04 17:25:50 -0400

    classList polyfill

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
-if ("ontouchstart" in document.documentElement) { 
-    document.querySelector(".hint").innerHTML = "<p>Tap on the left or right to navigate</p>";
-}
-
-impress().init();
-prettyPrint()
+Modernizr.load([
+    {
+        load: '//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js',
+        complete: function () {
+            if ( !window.jQuery ) {
+                Modernizr.load('js/vendor/jquery-1.7.2.min.js');
+            }
+        }
+    },
+    {
+        test: Modernizr.classList,
+        nope: ['js/vendor/classList.min.js'],
+        both: ['js/vendor/impress.js', 'js/vendor/prettify.js'],
+        complete: function () {
+            if ("ontouchstart" in document.documentElement) { 
+                document.querySelector(".hint").innerHTML = "<p>Tap on the left or right to navigate</p>";
+            }
+            impress().init();
+            prettyPrint();
+        }
+    }
+]);

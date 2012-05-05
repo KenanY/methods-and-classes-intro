7bba8c54224ccdbe2a3a94b5a8abefda0dc56f88
Kenan Yildirim <kenanyildirim@lavabit.com>
2012-05-04 20:56:07 -0400

    okay, maybe this is best

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ Modernizr.load([
             if ("ontouchstart" in document.documentElement) { 
                 document.querySelector(".hint").innerHTML = "<p>Tap on the left or right to navigate</p>";
             }
-            hljs.initHighlightingOnLoad();
+            $('pre code').each(function(i, e) {hljs.highlightBlock(e)});
             impress().init();
         }
     }

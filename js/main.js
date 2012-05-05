d3d1898b367b2d66be6561b2e54d820b4afff838
Kenan Yildirim <kenanyildirim@lavabit.com>
2012-05-04 20:43:05 -0400

    Google Prettify is aging; switch to highlight.js

--- js/main.js
+++ js/main.js
@@ -10,13 +10,13 @@ Modernizr.load([
     {
         test: Modernizr.classList,
         nope: ['js/vendor/classList.min.js'],
-        both: ['js/vendor/impress.js', 'js/vendor/prettify.js'],
+        both: ['js/vendor/impress.js', 'js/vendor/highlight.pack.js'],
         complete: function () {
             if ("ontouchstart" in document.documentElement) { 
                 document.querySelector(".hint").innerHTML = "<p>Tap on the left or right to navigate</p>";
             }
             impress().init();
-            prettyPrint();
+            hljs.initHighlightingOnLoad();
         }
     }
 ]);
